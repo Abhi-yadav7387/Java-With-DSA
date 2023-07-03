199fdc0fb16bb1633aa0ee5a529a2880e4e1c60e
Yadav Abhishek Bhim <123757127+Abhi-yadav7387@users.noreply.github.com>
2023-07-03 14:43:46 +0530

    Create Declaration.java

---
+++ Declaration.java
@@ -0,0 +1,13 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int age = 25;
+        double weight = 65.5;
+        String name = "alakh pandey";
+        
+        // Printing the values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+    }
+}
